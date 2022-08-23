74c256ca55cf86395ce7d1a7ed1a1b924244b8de
YrnCollo <kngenocollins@gmail.com>
2022-08-23 19:45:37 +0300

    print all the alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alpabets in range(ord('a'), ord('z') + 1):
-    print(chr(alpabets), end="")
+for alphabets in range(ord('a'), ord('z') + 1):
+    print("{:s}".format(chr(alphabets)), end="")
