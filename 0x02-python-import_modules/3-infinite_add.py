826ae93094a33ee4b75aa281c2aede8c4d1f3084
YrnCollo <kngenocollins@gmail.com>
2022-08-25 20:45:00 +0300

    made corrections

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
 
+
 def summing():
     add = 0
     for item, num in enumerate(argv):
         if item > 0:
             add = add + int(num)
     print(f"{add:d}")
+
 
 if __name__ == "__main__":
     summing()
