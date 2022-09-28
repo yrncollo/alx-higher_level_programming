9b5f1773549d1ea423538bc49c440d20a9cdd094
YrnCollo <kngenocollins@gmail.com>
2022-09-28 18:07:20 +0300

    function that adds a new attribute to an object if it’s possible

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+"100-my_int" module supplies the class MyInt
+"""
+
+
+class MyInt(int):
+    """rebel of an int"""
+    def __new__(cls, *args, **kwargs):
+        """create a new instance of the class"""
+        return super(MyInt, cls).__new__(cls, *args, **kwargs)
+
+    def __eq__(self, other):
+        """what was != is now =="""
+        return int(self) != other
+
+    def __ne__(self, other):
+        """what was == is now !="""
+        return int(self) == other
