540c10262c559b611fd32b62436e80f019822db7
yrncollo <kngenocollins@gmail.com>
2022-11-03 01:30:24 -0700

    script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+-- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server
+SHOW CREATE TABLE first_table;
