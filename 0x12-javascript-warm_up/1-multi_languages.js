cc44da4ac541c860081133a9fe4379208763f6f3
yrncollo <ngenocollins599@gmail.com>
2022-11-28 19:03:07 +0300

    printing in multiple line

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const firstLine = 'C is fun';
+const SecondLine = 'Python is cool';
+const thirdLine = 'Javascript is amazing';
+console.log(firstLine);
+console.log(SecondLine);
+console.log(thirdLine);
