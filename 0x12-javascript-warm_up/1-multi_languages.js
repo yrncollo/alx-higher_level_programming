1e8a41d816cc6e2bee68f9a6e851bf71349f64f9
yrncollo <ngenocollins599@gmail.com>
2022-11-28 19:08:28 +0300

    changed to one line

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-const firstLine = 'C is fun';
-const SecondLine = 'Python is cool';
-const thirdLine = 'Javascript is amazing';
-console.log(firstLine + '\n' + SecondLine + '\n' + thirdLine);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
